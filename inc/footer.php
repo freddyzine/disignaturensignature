df80706c22d17806a324542d5083639e5bdc8734
fredrick <freddyzine@gmail.com>
2020-08-21 16:11:03 +0100

    first commit

---
+++ inc/footer.php
@@ -0,0 +1,9 @@
+<!--Footer -->
+</div>
+<div class="navbar fixed-bottom navbar-light bg-light">
+<p> &copy Copyright 2020</p>
+<p>Website designed by Freddyzine</p>
+
+</div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,27 @@
+<?php include './inc/header.php';?>
+
+<div class="landingbg">
+ <img srcset="./img/6.svg 768w, ./img/7.svg 375w " sizes="(min-width: 320px) 375px" alt="Landing">
+
+ 
+    
+   <div class="btn">
+      <button  class="button"><span><a href="#"; style="text-decoration:none"; color="white" >COMING SOON!</a></span></button>
+  </div>
+  
+  <div class="line">
+      
+    </div>
+  <div class="pro">
+    
+      <div class="pro1">
+        <img src="./img/CR red.jpg" alt="Product 1">
+      </div>
+      <div class="pro1">
+        <img src="./img/CR2.jpg" alt="Product 2">
+    </div>
+</div>
+</div>
+
+
+<?php include './inc/footer.php';?>
